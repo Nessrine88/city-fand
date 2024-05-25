ba30b981cab4fad04ba3fd846fe9e09e6ab4156a
unknown <macherki.nessrine@yahoo.com>
2024-05-25 22:47:32 +0100

    added styles

--- src/pages/page1/FundsDetail.js
+++ src/pages/page1/FundsDetail.js
@@ -2,71 +2,70 @@ import React from 'react'
 
 const FundsDetail = () => {
   return (
-    <div className='my-5'>
-      <h3>Fund Details</h3>
-      <div className='w-[50%] grid grid-cols-3 my-5 '>
+    <div className='border-t-[1px] border-b-[1px] border-[#DDDDDD] my-[42px] py-[42px] '>
+      <h3 className='class="font-plus-jakarta-sans text-[20px] font-semibold leading-[28px] tracking-[-0.01em] text-left"'>Fund Details</h3>
+      <div className='w-[70%] grid grid-cols-3 my-5 '>
         <div>
-        <p className='flex'>
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
             Strategy
-           <img src='/images/exclamation.png' />
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>Growth</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Fund Status
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>Ramping Up</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Management Fee
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>1.5% per year</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Term
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p>Open-ended</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Dividends
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>Event-based</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Target Return
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>18.0% IRR</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Acquisition Fee
+           <img className='ml-1 w-[20px] h-[20px]'src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>1.0%</p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+            
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '></p>
       </div>
       <div>
-        <p className='flex'>
-            Strategy
-           <img src='/images/exclamation.png' />
+        <p className='flex text-[#989B9F] text-[14px] font-normal leading-[21px] tracking-[-0.5px] text-left'>
+        Tax Form
+           <img className='ml-1 w-[20px] h-[20px]' src='/images/exclamation.png' />
         </p>
-        <p>Growth</p>
+        <p className='font-plus-jakarta-sans text-[20px] font-semibold leading-[26px] tracking-[-0.24px] text-left text-[#303030] pb-[26px] pt-[8px]  '>1099-DIV</p>
       </div>
       </div>
     </div>
